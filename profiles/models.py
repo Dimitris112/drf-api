1430dbc953ce55e525bd5a057415849d12b62292
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-16 23:33:41 +0300

    Add profile serializer & list view

--- profiles/models.py
+++ profiles/models.py
@@ -21,6 +21,4 @@ class Profile(models.Model):
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(owner=instance)
-
-post_save.connect(create_profile, sender=User)
+        post_save.connect(create_profile, sender=User)
